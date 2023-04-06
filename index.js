6c98eae6806c8c99ccecd1c83bd7cac4ee0c0946
MSc Luis A. Hernández Elias <luishe72@yahoo.com>
2023-04-06 19:08:39 -0300

    Se actualizan las principales rutas

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.send("Lista de productos");
+});
+app.get("/:id", (req, res) => {
+  res.send(`Mostrar el producto`);
+});
+app.post("/", (req, res) => {
+  console.log(` Se creo un nuevo producto con el codigo ${req.body.codigo}`);
+  res.send(req.body);
+});
+app.put("/:id", (req, res) => {
+  console.log(` Se actualizo el producto con el codigo ${req.params.id}`);
+  res.send(req.params);
+});
+app.delete("/:id", (req, res) => {
+  res.send(` Se elimino el producto con el codigo ${req.params.id}`);
 });
 
 app.listen("3000", () => {
