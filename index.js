63cebe4dcb54b64f3af35e27b78f97e3347de282
MSc Luis A. Hernández Elias <luishe72@yahoo.com>
2023-04-06 21:13:32 -0300

    Actualizando funcionalidades del CRUD

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Producto = require("./model/producto");
+
+let listaProductos = [];
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -10,21 +14,71 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Lista de productos");
+  res.send(listaProductos);
 });
-app.get("/:id", (req, res) => {
-  res.send(`Mostrar el producto`);
+app.get("/:codigo", (req, res) => {
+  // Buscar el producto en la lista de productos
+  const productoEncontrado = listaProductos.find(
+    (producto) => producto.codigo === req.params.codigo
+  );
+
+  // Verificar si se encontró un producto con ese código
+  if (productoEncontrado) {
+    // Crear una nueva instancia de la clase Producto con los valores del producto encontrado
+    res.send(productoEncontrado);
+  } else {
+    console.log("Producto no encontrado");
+    res.send("500");
+  }
 });
 app.post("/", (req, res) => {
+  //extraemos los valores del request body
+  let codigo = req.body.codigo;
+  let nombre = req.body.nombre;
+  let descripcion = req.body.descripcion;
+  let precio = req.body.precio;
+  let costo = req.body.costo;
+  let stock = req.body.stock;
+
+  //actualizamos la variable product
+  const product = new Producto(
+    codigo,
+    nombre,
+    precio,
+    costo,
+    descripcion,
+    stock
+  );
+
+  //adicionamos los valores a la lista
+  listaProductos.push(product);
+
   console.log(` Se creo un nuevo producto con el codigo ${req.body.codigo}`);
-  res.send(req.body);
+  res.send(product);
 });
-app.put("/:id", (req, res) => {
-  console.log(` Se actualizo el producto con el codigo ${req.params.id}`);
-  res.send(req.params);
+app.put("/:codigo", (req, res) => {
+  const index = listaProductos.findIndex(
+    (producto) => producto.codigo === req.params.codigo
+  );
+  if (index !== -1) {
+    listaProductos[index].codigo = req.body.codigo;
+    listaProductos[index].nombre = req.body.nombre;
+    listaProductos[index].descripcion = req.body.descripcion;
+    listaProductos[index].precio = req.body.precio;
+    listaProductos[index].costo = req.body.costo;
+    listaProductos[index].stock = req.body.stock;
+    console.log("Producto actualizado:", listaProductos[index]);
+    res.send("Producto actualizado:");
+  } else {
+    console.log("Producto no encontrado");
+    res.send("500");
+  }
 });
-app.delete("/:id", (req, res) => {
-  res.send(` Se elimino el producto con el codigo ${req.params.id}`);
+app.delete("/:codigo", (req, res) => {
+  listaProductos = listaProductos.filter(
+    (producto) => producto.codigo !== req.params.codigo
+  );
+  res.send(` Se elimino el producto con el codigo ${req.params.codigo}`);
 });
 
 app.listen("3000", () => {
